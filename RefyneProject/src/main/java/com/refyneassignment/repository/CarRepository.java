64a81296f903b6e0dac29863a53a509bc955238d
premu0007 <premsagar350@gmail.com>
2021-04-13 11:38:56 +0530

    repository added
    newly added file were added

---
+++ RefyneProject/src/main/java/com/refyneassignment/repository/CarRepository.java
@@ -0,0 +1,18 @@
+package com.refyneassignment.repository;
+
+import com.refyneassignment.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long > {
+
+    Car findByCarId(Long carId);
+
+//new message
+//    @Query("select * from Car c where c.model='honda'")
+//    List<Car> findAllCarsByModel(String model);
+}
---
+++ RefyneProject/src/main/java/com/refyneassignment/service/CarService.java
@@ -0,0 +1,54 @@
+package com.refyneassignment.service;
+
+import com.refyneassignment.entity.Car;
+import com.refyneassignment.repository.CarRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Slf4j
+public class CarService {
+
+    @Autowired
+    private CarRepository carRepository ;
+    
+    public Car saveNewCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    public Car findByCarId(Long carId) {
+        return carRepository.findByCarId(carId);
+    }
+
+//    public Car updateCar(Car car) {
+//        return  carRepository.update(car);
+//    }
+
+    public void deleteCar(Long carId) {
+          carRepository.deleteById(carId);
+    }
+
+    public List<Car> findAllCars() {
+      return  carRepository.findAll();
+    }
+
+    public List<Car> searchCarsbyDate(Date fromDateTime, Date toDateTime) {
+        List<Car>  result= new ArrayList<>();
+        try{
+            List<Car> newList= carRepository.findAll();
+            for( int i=0; i<newList.size(); i++){
+                if(newList.get(i).getFromDateTime().after(fromDateTime) && newList.get(i).getToDateTime().before(toDateTime)){
+                    result.add(newList.get(i));
+                }
+            }
+        }catch (Exception e){
+            log.info("Error inside searchCarsbyDate: cause={}", e.getMessage());
+        }
+        return result;
+    }
+}
